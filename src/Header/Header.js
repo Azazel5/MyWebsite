3d0c43a13d1b7efd00bb782866b7745ee2bc3ea3
Azazel5 <subhanga2013@gmail.com>
2020-12-23 08:45:15 -0500

    Finished header section with svgs

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,4 +1,5 @@
-import CoolPic from '../assets/images/cool_pic.jpg'
+import CoolPic from '../assets/images/cool_pic.jpeg'
+import Sprite from '../assets/SVG/symbol-defs.svg'
 
 const Header = props => {
     return (
@@ -8,10 +9,39 @@ const Header = props => {
                 <h1 className="container__title">Hi, my name is Subhanga Upadhyay.</h1>
                 <p>
                     I am a computer science student who loves the process of learning and getting better at things. My
-                    pursuit of gaining experience in the craft of programming has allowed me the opprotunity of working 
+                    pursuit of gaining experience in the craft of programming has allowed me the opprotunity of working
                     with many different technologies. <br />
-                    Currently I am having a blast working with: 
+                    Tools I currently enjoy working with:
                 </p>
+                <div className="container__blast-grid">
+                    <div className="container__blast-grid__element">
+                        React
+                        <svg className="svg-icon"><use xlinkHref={`${Sprite}#icon-react`}></use></svg>
+                    </div>
+
+                    <div className="container__blast-grid__element">
+                        Django
+                        <svg className="svg-icon"><use xlinkHref={`${Sprite}#icon-django`}></use></svg>
+                    </div>
+
+                    <div className="container__blast-grid__element">C programming
+                        <svg className="svg-icon"><use xlinkHref={`${Sprite}#icon-c`}></use></svg>
+                    </div>
+
+                    <div className="container__blast-grid__element">
+                        SCSS
+                        <svg className="svg-icon"><use xlinkHref={`${Sprite}#icon-sass`}></use></svg>
+                    </div>
+
+                    <div className="container__blast-grid__element">
+                        TensorFlow
+                        <svg className="svg-icon"><use xlinkHref={`${Sprite}#icon-tensorflow`}></use></svg>
+                    </div>
+
+                    <div className="container__blast-grid__element">Linux
+                        <svg className="svg-icon"><use xlinkHref={`${Sprite}#icon-tux`}></use></svg>
+                    </div>
+                </div>
             </div>
         </div>
     )
--- src/Navigation/Navigation.js
+++ src/Navigation/Navigation.js
@@ -3,7 +3,6 @@ const Navigation = props => {
         <div className="navigation-bar">
             <div className="s">Welcome</div>
             <div className="navigation-bar__col">Portfolio</div>
-            <div className="navigation-bar__col">About</div>
             <div className="navigation-bar__col">Contact</div>
             <div className="navigation-bar__col">Blog</div>
         </div>
