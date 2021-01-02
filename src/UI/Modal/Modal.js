5df7c9d855a06afa6eac16a5bb94104f60725911
Azazel5 <subhanga2013@gmail.com>
2021-01-01 19:20:28 -0500

    Modal inner content foundations created + closing button styled

--- src/UI/Modal/Modal.js
+++ src/UI/Modal/Modal.js
@@ -1,20 +1,33 @@
 import { useRef } from 'react'
 import BackgroundClickHook from '../../Hooks/BackgroundClickHook/BackgroundClickHook'
+import Sprite from '../../Assets/SVG/symbol-defs.svg'
 
 const Modal = props => {
     // State and props variables 
     const { modalOpen, onModalCloseHandler } = props
     const modelContainerRef = useRef()
     BackgroundClickHook(modelContainerRef, onModalCloseHandler)
+    console.log(modalOpen)
 
     return (
         Object.keys(modalOpen).length > 0 &&
         <div className="modal">
             <div className="modal__container" ref={modelContainerRef}>
-                <button className="modal__close-button" onClick={onModalCloseHandler}>Close</button>
+                <button className="modal__close-button" onClick={onModalCloseHandler}>
+                    <svg><use xlinkHref={`${Sprite}#icon-circle-with-cross`}></use></svg>
+                </button>
+
                 <div className="centered-box">
-                    <h1>This website</h1>
-                    <svg className="modal__star-icon" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="star" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" data-fa-i2svg=""><path fill="currentColor" d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path></svg>
+                    <h1>{modalOpen['Name']}</h1>
+                    <div className="modal__symbol">
+                        <div className="modal__symbol--line"></div>
+                        <svg className="modal__symbol--star-icon" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="star" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" data-fa-i2svg=""><path fill="currentColor" d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path></svg>
+                        <div className="modal__symbol--line"></div>
+                    </div>
+                    <div>{modalOpen['Description']}</div>
+                    <a href="#?">{modalOpen['Github Repository']}</a>
+                    <div>{modalOpen['Technologies Used']}</div>
+                    <div>{modalOpen['Notes/New Reflection'] && modalOpen['Notes/New Reflection']}</div>
                 </div>
             </div>
         </div>
